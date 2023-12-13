6cb0fbf6f75134a28e92ba78194c0a6f93ddde1d
icklokore <bartjeddd@gmail.com>
2023-12-13 20:19:29 +0100

    adds basic js, css and html for demo site

--- assets/js/loadPage.js
+++ assets/js/loadPage.js
@@ -1,6 +1,28 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", init);
 
-function init() {
+function init(e) {
+    e.preventDefault();
+    const user = JSON.parse(localStorage.getItem("user"));
+    if(user === "name"){
+        document.querySelector("#SecretLink").classList.add("hidden");
+    } else {
+        document.querySelector("#SecretLink").classList.remove("hidden");
+    }
+    document.querySelector("#set").addEventListener("click", clickSetHandler);
+    document.querySelector("#remove").addEventListener("click", clickRemoveHandler);
+}
 
+function clickSetHandler(e){
+    e.preventDefault;
+    saveUserInLocalStorage("name");
+    //refresh page
+    location.reload();
+}
+
+function clickRemoveHandler(e){
+    e.preventDefault;
+    removeUserFromLocalStorage();
+    //refresh page
+    location.reload();
 }
--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function getUserFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+function saveUserInLocalStorage(user) {
+    if(localStorage.getItem("user") === null){
+        localStorage.setItem("user", JSON.stringify(user));
+    }
+}
+
+function removeUserFromLocalStorage(){
+    localStorage.removeItem("user");
+}
